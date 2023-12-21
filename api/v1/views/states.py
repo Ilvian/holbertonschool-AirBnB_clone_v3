ea45d78c84f64de2979b05f38508e27ec4e8c2f9
Ilvian <d.ilvian1993@gmail.com>
2023-12-21 01:44:23 -0800

    states

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
-"""states"""
+"""
+states
+"""
 from api.v1.views import app_views
 from flask import jsonify, abort, request
 from models import storage
@@ -10,14 +12,18 @@ import uuid
 
 @app_views.route('/states/', methods=['GET'])
 def list_states():
-    '''Retrieves a list of all State objects'''
+    '''
+    Retrieves a list of all State objects
+    '''
     list_states = [obj.to_dict() for obj in storage.all("State").values()]
     return jsonify(list_states)
 
 
 @app_views.route('/states/<state_id>', methods=['GET'])
 def get_state(state_id):
-    '''Retrieves a State object'''
+    '''
+    Retrieves a State object
+    '''
     all_states = storage.all("State").values()
     state_obj = [obj.to_dict() for obj in all_states if obj.id == state_id]
     if state_obj == []:
@@ -27,7 +33,9 @@ def get_state(state_id):
 
 @app_views.route('/states/<state_id>', methods=['DELETE'])
 def delete_state(state_id):
-    '''Deletes a State object'''
+    '''
+    Deletes a State object
+    '''
     all_states = storage.all("State").values()
     state_obj = [obj.to_dict() for obj in all_states if obj.id == state_id]
     if state_obj == []:
@@ -42,7 +50,9 @@ def delete_state(state_id):
 
 @app_views.route('/states/', methods=['POST'])
 def create_state():
-    '''Creates a State'''
+    '''
+    Creates a State
+    '''
     if not request.get_json():
         abort(400, 'Not a JSON')
     if 'name' not in request.get_json():
@@ -57,7 +67,9 @@ def create_state():
 
 @app_views.route('/states/<state_id>', methods=['PUT'])
 def updates_state(state_id):
-    '''Updates a State object'''
+    '''
+    Updates a State object
+    '''
     all_states = storage.all("State").values()
     state_obj = [obj.to_dict() for obj in all_states if obj.id == state_id]
     if state_obj == []:
