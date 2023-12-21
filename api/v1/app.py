7c8a8f21a57c9ac088969e09349bff48e18deda8
Ilvian <d.ilvian1993@gmail.com>
2023-12-21 12:21:18 -0800

    app.py

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
-"""app"""
+'''
+app
+'''
 from flask import Flask, make_response, jsonify
 from models import storage
 from api.v1.views import app_views
@@ -17,13 +19,17 @@ app.register_blueprint(app_views)
 
 @app.teardown_appcontext
 def tear(self):
-    ''' closes storage engine '''
+    '''
+    closes storage engine
+    '''
     storage.close()
 
 
 @app.errorhandler(404)
 def not_found(error):
-    ''' handles 404 error and gives json formatted response '''
+    '''
+    handles 404 error and gives json formatted response
+    '''
     return make_response(jsonify({'error': 'Not found'}), 404)
 
 if __name__ == '__main__':
